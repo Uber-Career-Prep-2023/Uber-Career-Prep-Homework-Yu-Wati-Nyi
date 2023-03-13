5b494c48e4899b20d9e6c7db85fcf72590872f7e
Yu Wati Nyi <yuwatinyi@Yus-MacBook-Air.local>
2023-03-12 20:04:30 -0400

    Question 5 submission

--- Assignment 1/ShortestSubString.java
+++ Assignment 1/ShortestSubString.java
@@ -27,45 +27,47 @@ public class ShortestSubString {
 
  public int shortestsubstring(String input, String inputCheck){
     
-    
+    //create arraylist to hold each substring
     ArrayList<Character> stringHolder = new ArrayList<Character>();
     
+    //string to hold shortest string
     String outputString = "";
 
+    //array to keep track of occurences of each character in the input check string
     int[] counter = new int[inputCheck.length()];
-    int size = 100;
+    int size = 100; // initial size
 
-    
+    //start new substring with starting point one index later
     for(int k = 0; k< input.length(); k++){
-        counter = new int[inputCheck.length()];
+        counter = new int[inputCheck.length()]; //set all occurences to zero
         
-        for(int i = 0 +k ; i< input.length(); i++){
+        for(int i = 0 +k ; i< input.length(); i++){ //loop through input
             
             int count = 0;
-            for(int j =0; j< inputCheck.length(); j++){
-                if(input.charAt(i)== inputCheck.charAt(j) && counter[j] <1 ){
+            for(int j =0; j< inputCheck.length(); j++){ 
+                if(input.charAt(i)== inputCheck.charAt(j) && counter[j] <1 ){ //iterate if there is an occurence and less than one so that there is no double count
                     counter[j]++;
                     break;
                 }
             }
-            for(int j =0; j< inputCheck.length(); j++){
+            for(int j =0; j< inputCheck.length(); j++){ //count which characters in inputcheck are included
 
                 if(counter[j] > 0){
                     count++;
                 }
             }
-            stringHolder.add(input.charAt(i));
+            stringHolder.add(input.charAt(i)); //add character to arraylist
             
-            if (count == inputCheck.length()){
+            if (count == inputCheck.length()){ //if all characters in inputCheck are included in that order
                 
-                if(stringHolder.size() < size){
-                    outputString = CharacterstoString(stringHolder);
+                if(stringHolder.size() < size){//update with new shortest string
+                    outputString = CharacterstoString(stringHolder); //make a string
 
-                    size = outputString.length();
+                    size = outputString.length();// set new size for comparison
                 }
-                stringHolder.clear();
+                stringHolder.clear(); //clear the arraylist to repeat process with new substrings
                 
-                break;
+                break; //start at next k value, do not go through the remaining substrings in current k value
                 
 
             }
